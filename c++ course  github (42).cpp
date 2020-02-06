07d5976e707ca1e7d114e1db244bc4a53844788d
MUHAMMAD ADIL RAZA <51850381+madilraza@users.noreply.github.com>
2020-02-06 15:24:46 +0500

    Add files via upload

---
+++ c++ course  github (42).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<" enter the value";
+	cin>>x; if(x>100)
+	cout<<" the number is greater then 100 ";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
